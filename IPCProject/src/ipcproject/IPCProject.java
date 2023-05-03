98d7b31830becc9f6143ddbd2cc7b2eac6afbd6f
Aronxuu <90319535+Aronxuu@users.noreply.github.com>
2023-05-03 17:31:17 +0200

    Keloke ermano
    Hola premo

--- IPCProject/src/ipcproject/IPCProject.java
+++ IPCProject/src/ipcproject/IPCProject.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
 
 
 public class IPCProject extends Application {
-    //keloke ermano
+    //keloke ermano as
     @Override
     public void start(Stage stage) throws Exception {
         //======================================================================
