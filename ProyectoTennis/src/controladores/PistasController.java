be867836957d022b5580306caccc240565e99bf1
Aronxuu <90319535+Aronxuu@users.noreply.github.com>
2023-05-04 17:25:49 +0200

    Actualización fondo pistas.fxml

---
+++ ProyectoTennis/src/controladores/PistasController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author aronx
+ */
+public class PistasController implements Initializable {
+
+    @FXML
+    private Button button16;
+    @FXML
+    private ImageView img11;
+    @FXML
+    private Button button1;
+    @FXML
+    private ImageView img1;
+    @FXML
+    private Button button11;
+    @FXML
+    private ImageView img2;
+    @FXML
+    private Button button12;
+    @FXML
+    private ImageView img5;
+    @FXML
+    private Button button13;
+    @FXML
+    private ImageView img4;
+    @FXML
+    private Button button14;
+    @FXML
+    private ImageView img3;
+    @FXML
+    private Button button15;
+    @FXML
+    private ImageView img6;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
