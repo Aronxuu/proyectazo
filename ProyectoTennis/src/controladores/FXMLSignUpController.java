0253ce79b255b40baaf5ffd519c0dca730c80349
IvanCrespoG <131807811+IvanCrespoG@users.noreply.github.com>
2023-05-13 16:49:27 +0200

    Add files via upload

--- ProyectoTennis/src/controladores/FXMLSignUpController.java
+++ ProyectoTennis/src/controladores/FXMLSignUpController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controladores;
 
 
@@ -20,6 +15,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -46,43 +42,61 @@ public class FXMLSignUpController implements Initializable {
     @FXML
     private Label pwdError;
     @FXML
-    private Button Register;
-    @FXML
     private Button Login;
     
     private Club club;
     
     public String login;
+    public String pasword;
     
+    @FXML
+    private Label emailError;
+    
+    @FXML
     private void handleRegister(ActionEvent event) throws IOException {
-    FXMLLoader loader = new FXMLLoader(getClass().getResource("register.fxml"));
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/archivosfxml/register.fxml"));
     Parent root = loader.load();
-    Stage stage = new Stage();
+    Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
     Scene scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
+
     }
+    @FXML
     private void handleLogin(ActionEvent event) throws IOException {
-        String pasword = pwdInput.getText();
+        pasword = pwdInput.getText();
         login = emailInput.getText();
+        System.out.println(pasword+login);
         try {
             club = getInstance();
-            club.setInitialData();
+
         } catch (ClubDAOException ex) {
             Logger.getLogger(FXMLSignUpController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        try{
        Member b = club.getMemberByCredentials(login, pasword);
-       
        if(b == null){
+           
             pwdError.setVisible(true);
        }else{
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("pistas.fxml"));
-            Parent root = loader.load();
-            Stage stage = new Stage();
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/archivosfxml/pistas.fxml"));
+        loader.setControllerFactory(controllerClass -> {
+
+        PistasController controller = new PistasController();
+        controller.setLogin(login);
+        return controller;
+    
+        });
+        Parent root = loader.load();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
        }
+        }catch(Exception e){
+            pwdError.setVisible(true);
+        }
+       
     }
    
     private void hideErrorMessage()
@@ -90,15 +104,22 @@ public class FXMLSignUpController implements Initializable {
         pwdError.setVisible(false);
     }
     
+    @FXML
     private void handleButton(ActionEvent event) throws IOException {
-    FXMLLoader loader = new FXMLLoader(getClass().getResource("pistassinlogin.fxml"));
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/archivosfxml/pistassinlogin.fxml"));//DEBERÍA SER pistassinlogin.fxml pero pongo pistas.fxml para testear
     Parent root = loader.load();
-    Stage stage = new Stage();
+    Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
     Scene scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
     }
 
+    public String getLogin(){
+        return login;
+    }
+    public String getPasword(){
+        return pasword;
+    }
     
     //=========================================================
     // you must initialize here all related with the object 
